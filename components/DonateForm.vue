c05b17a88d5b4d551034443421a345b833f832c1
Roger Gutiérrez <127939943+Rogercodi@users.noreply.github.com>
2023-10-23 20:11:14 +0200

    Add logic to components

--- components/DonateForm.vue
+++ components/DonateForm.vue
@@ -1,19 +1,52 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const donateStore = useDonateStore();
+const overlayStore = useOverlayStore();
+const orderStore = useOrdersStore();
+
+const postalCode = ref();
+const firstname = ref();
+
+//Capture values of Donate Form to Donate Store
+watch(firstname, (current) => {
+  donateStore.setFirstname(current)
+});
+
+watch(postalCode, (current) => {
+  donateStore.setPostalCode(current)
+})
+
+const hideOverlay = () => {
+  overlayStore.toogleDisplay(false)
+};
+
+const onSubmit = async () => {
+  //Donate order
+  await donateStore.donateOrder();
+  donateStore.$reset();
+  //Update orders
+  await orderStore.getOrders();
+  //Reset Donate form
+  overlayStore.toogleDisplay(false);
+  postalCode.value = '';
+  firstname.value = '';
+}
+
 
 
 </script>
-
 <template >
-  <div class="overlay">
+  <div v-if="overlayStore.display" class="overlay">
     <div class="card col-6 overlay-content">
       <div class="card-header">
         Donate Form
       </div>
       <div class="card-body">
-        <form class="row g-3">
+        <form @submit.prevent="onSubmit" class="row g-3">
           <div class="col-8">
             <label for="pc" class="form-label col-8">Postal code</label>
-            <select  class="rounded p-2 col-12" id="pc" required>
+            <select v-model="postalCode" class="rounded p-2 col-12" id="pc" required>
               <option value="" selected disabled>Select Postal Code</option>
               <option value="28005">28005</option>
               <option value="08001">08001</option>
@@ -22,10 +55,10 @@
           </div>
           <div class="col-8">
             <label for="firstname" class="form-label">Firstname</label>
-            <input type="text" class="form-control border-dark" id="firstname" required>
+            <input v-model="firstname" type="text" class="form-control border-dark" id="firstname" required>
           </div>
           <div class="card-footer d-flex justify-content-end align-items-center ">
-            <button  class="btn btn-secondary mx-4">Close</button>
+            <button @click="hideOverlay()" class="btn btn-secondary mx-4">Close</button>
             <button type="submit" class="btn btn-primary">Save changes</button>
           </div>
         </form>
--- components/FilteredOrdersList.vue
+++ components/FilteredOrdersList.vue
@@ -1,4 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const donateStore = useDonateStore();
+const overlayStore = useOverlayStore();
+const orderStore = useOrdersStore();
+//Initialize orders
+await orderStore.getOrders();
+const orders = computed(() => orderStore.orders);
+
+const filter = ref('undefined');
+
+//Render Filtered Orders
+watch(filter, async (current) => {
+    orderStore.setFilter(current);
+    await orderStore.getOrders()
+});
+
+const showOverlay = (id: number) => {
+    //Display donate form and set order id for Url params
+    overlayStore.toogleDisplay(true);
+    donateStore.setId(JSON.stringify(id))
+};
 
 </script>
 
@@ -10,7 +32,7 @@
             </div>
             <div class="col-md-3 mb-2">
                 <form>
-                    <select class="form-select border-dark fs-10">
+                    <select v-model="filter" class="form-select border-dark fs-10">
                         <option value="undefined" selected>All</option>
                         <option value="normal">Normal</option>
                         <option value="donation">Donation</option>
@@ -19,13 +41,14 @@
             </div>
         </div>
     </div>
-    <div class="row">
+    <div v-for="order of orders" class="row">
         <div class="order col-4 border pt-4">
-            <p class="mb-0">sku:  </p>
-            <p class="mb-0">type: </p>
-            <p class="mb-0">quantity:  </p>
-            <p class="mb-4">firstname:  </p>
-            <button class="btn btn-primary my-3 d-block mx-auto">Donate</button>
+            <p class="mb-0">sku: {{ order.attributes.order_items.data[0].attributes.sku }} </p>
+            <p class="mb-0">type: {{ order.attributes.type }} </p>
+            <p class="mb-0">quantity: {{ order.attributes.order_items.data[0].attributes.quantity }} </p>
+            <p class="mb-4">firstname: {{ order.attributes.order_meta.data.attributes.shipping_firstname }} </p>
+            <button v-if="order.attributes.type !== 'donation'" @click="showOverlay(order.id)"
+                class="btn btn-primary my-3 d-block mx-auto">Donate</button>
         </div>
     </div>
 </template>
